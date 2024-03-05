dff85edcbf4f067938407f2b78e0225b76fed740
Ankit Raj <ankitrajbaj@gmail.com>
2024-03-06 00:16:28 +0530

    Configured tailwind and handle props.

--- 03tailwindprops/src/App.js
+++ 03tailwindprops/src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 import Card from './Components/Card'
 
 function App() {
+  let myObj = {
+    username: "ankit",
+    age: 21
+  }
+  let newArr = [1,2,3]
+
   return (
+    
     <>
       <h1 className='relative-h-400px bg-green-400 text-black p-4 rounded-xl mb-4 text-center'>Tailwind test</h1>
 
-      <Card />
+      <Card username="Ankit" btnText='Click Me!' />
+      <Card username="charlie" />
       
       
 
