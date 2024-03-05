f209370a001cac8a1d7857d2c9a3cfa6197cebea
Ankit Raj <ankitrajbaj@gmail.com>
2024-03-05 22:48:45 +0530

    card

--- 03tailwindprops/src/App.js
+++ 03tailwindprops/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Card from './Components/Card'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='relative-h-400px bg-green-400 text-black p-4 rounded-xl mb-4 text-center'>Tailwind test</h1>
+
+      <Card />
+      
+      
+
+    </>
   );
 }
 
